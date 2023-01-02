c2596b3c7c6f120b853d881f2fab4ecb47699139
Bhupendra Singh <45242141+itsbhupendrasingh@users.noreply.github.com>
2023-01-02 22:36:53 +0530

    Add files via upload

---
+++ led_message_moving_display_ESP32/led_message_moving_display_ESP32.ino
@@ -0,0 +1,73 @@
+#define BLYNK_TEMPLATE_ID "Template ID"
+#define BLYNK_DEVICE_NAME "Device name"
+#define BLYNK_AUTH_TOKEN "Auth Token"
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#include <SoftwareSerial.h> 
+// Install software library for the ESP32. Library link is available in the video description
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "********"; // Change your Wifi/ Hotspot Name
+char pass[] = "********"; // Change your Wifi/ Hotspot Password
+
+int x = 0;
+bool text = false;
+String textIn = "";
+String text_speed = "";
+BLYNK_WRITE(V0)
+{
+  textIn = param.asStr();
+  if (textIn) // working for text Input Widget
+  {
+    text = true;
+    x = 0;
+    Serial2.print(String("!") + textIn + String("\r\n"));
+    Serial.print(String("!") + textIn + String("\r\n"));
+    text = false;
+    Serial2.flush();
+  }
+}
+
+BLYNK_WRITE(V1)
+{
+  text_speed = param.asStr();
+/* Control the speed of the scrolling texts
+ *  0xF0 = Static Mode, No scrolling
+ *  0xF1 to 0xFF = You can send any hex values frmo 0xF1 to 0xFF, where 0xF1 is the fastest and 0xFF is the
+ *  lowest scrolling speed. Default power up speed is medium at 0xFA 
+ */
+  if (text_speed == "0") // working for text Input Widget
+  {
+    Serial2.write(0xF0); 
+    Serial.print(text_speed);
+  }
+  if (text_speed == "50") // working for text Input Widget
+  {
+    Serial2.write(0xFF); 
+    Serial.print(text_speed);
+  } 
+  if (text_speed == "100") // working for text Input Widget
+  {
+    Serial2.write(0xF1); 
+    Serial.println(text_speed);
+  } 
+  delay(100);
+  Serial2.flush();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
+  Blynk.begin(auth, ssid, pass);
+  delay(1000);
+}
+
+void loop()
+{
+  Blynk.run();
+}
